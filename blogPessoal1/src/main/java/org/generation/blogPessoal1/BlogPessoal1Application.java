fde94519f3d34e87cec5c65428536ab274d5ed29
unknown <joaovitor02cp@gmail.com>
2021-03-17 16:26:46 -0300

    Atividade Spring

---
+++ blogPessoal1/src/main/java/org/generation/blogPessoal1/BlogPessoal1Application.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoal1Application.class, args);
+	}
+
+}
